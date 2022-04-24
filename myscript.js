d34b211af65d600a930c2a2684abfad25f7ce99d
Winifred Asante <winifredasante15@gmail.com>
2022-04-24 13:50:39 +0000

    js commit

---
+++ myscript.js
@@ -0,0 +1,62 @@
+let coins = 56
+let stars = 30
+
+console.log(coins + stars)
+
+console.log(coins/stars)
+
+let lowscore = 400
+var highscore = 70
+let lev = 5
+
+// console.log(highscore <= lowscore)
+
+// console.log(highscore >= lowscore)
+
+if (highscore > lowscore) {
+    console.log(answer)
+}
+else{
+    (console.log(negetive))
+} 
+
+
+let red = "i am red"
+let blue = "i am blue"
+let allcolors = "i am all"
+
+if(red != blue){
+    console.log(age)
+}
+
+else(console.log("you are not sure...are you?"))
+
+
+let num = 155
+let height = 155
+let all = 500
+
+if(score <= lowscore){
+    console.log(answer)
+}
+else(document.write("very poor"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
